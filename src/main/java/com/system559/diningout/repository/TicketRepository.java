719abb67ef8c9c5cd275c1e6e6491b73ab37940b
James Osterhage <josterhage@system559.com>
2022-03-15 19:00:16 -0700

    added findByGuestId query

--- src/main/java/com/system559/diningout/repository/TicketRepository.java
+++ src/main/java/com/system559/diningout/repository/TicketRepository.java
@@ -3,7 +3,12 @@ package com.system559.diningout.repository;
 import com.system559.diningout.model.Guest;
 import com.system559.diningout.model.Ticket;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.Optional;
 
 public interface TicketRepository extends MongoRepository<Ticket, String> {
     void deleteByGuest(Guest guest);
+    @Query("{'guest._id':ObjectId('?0')}")
+    Optional<Ticket> findByGuestId(String guestId);
 }
