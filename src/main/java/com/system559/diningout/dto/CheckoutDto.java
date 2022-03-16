a9e9dde9d7dd70b3c4cfed64bf24166c0c755da9
James Osterhage <josterhage@system559.com>
2022-03-15 20:49:24 -0700

    refactored to enable requirement change involving ticket pricing

--- src/main/java/com/system559/diningout/dto/CheckoutDto.java
+++ src/main/java/com/system559/diningout/dto/CheckoutDto.java
@@ -3,13 +3,14 @@ package com.system559.diningout.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @Builder
 public class CheckoutDto {
     private String clientSecret;
-    private String tierName;
-    private long tierPrice;
-    private long quantity;
+    private List<TicketTierDto> ticketTiers;
     private long fee;
+    private long total;
     private long created;
 }
