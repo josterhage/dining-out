827723d25b23fdaccaf069613cad66f8ed518475
James Osterhage <josterhage@gmail.com>
2022-12-30 23:31:03 -0700

    changes deleteByGuest @Query syntax

--- src/main/java/com/system559/diningout/repository/TicketRepository.java
+++ src/main/java/com/system559/diningout/repository/TicketRepository.java
@@ -9,6 +9,6 @@ import java.util.Optional;
 
 public interface TicketRepository extends MongoRepository<Ticket, String> {
     void deleteByGuest(Guest guest);
-    @Query("{'guest._id':ObjectId('?0')}")
+    @Query("{'guest._id':ObjectId(guestId)}")
     Optional<Ticket> findByGuestId(String guestId);
 }
